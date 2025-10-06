84e24e9e3ce7c34faa4e41522f19f309e240ed8b
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-10-06 09:30:57 +0500

    lec3

---
+++ Lecture_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    unsigned int roomNo;
+    roomNo = 10;
+    cout << "helle \a words" << endl;
+    cout << " your name";
+
+    return 0;
+}
+
