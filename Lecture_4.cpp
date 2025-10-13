1684778fd10fbf469e826673ca33b4fb166c52ac
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-10-13 09:52:31 +0500

    lec 4

---
+++ Lecture_4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int x;
+    char y = 'a';
+    string z= "your name";
+    cout<<sizeof(y)<<endl;
+    cout<<sizeof(z)<<endl;
+   
+    double firstnumber;
+    firstnumber = 22.0/7;
+    cout << firstnumber;
+    bool condition;
+    condition = true;
+    return 0;
+}
