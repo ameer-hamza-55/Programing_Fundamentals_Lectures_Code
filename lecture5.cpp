563f6d6a38f5f6b377491a29c42ac880f3b80812
Ameer Hamza <186324306+ameer-hamza-55@users.noreply.github.com>
2025-10-20 09:36:17 +0500

    lecture 6

---
+++ lecture5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int length;
+    int width;
+    cin >> length>>width;
+    int result = pow(3, 2);
+    
+
+    return 0;
+}
---
+++ lecture6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    char mycahrvariable = 'A';
+    short int a = 5;
+    float b = 6.968789;
+    cout << a * mycahrvariable<<endl;
+    cout << a * b << endl;
+    float result = a / b;
+    cout << result << endl;
+    short int c = -32769;
+    cout << c << endl;
+    int result2= static_cast<int>(6.7)/ 6;
+    int d, e, f, g;
+    f = e = f = g = 5;
+    e = e + 10;
+    e += 10;
+    e /= 5;
+    cout<<left << setw(5) << a << setw(5) << b << c << endl;
+    cout <<fixed<<setprecision(2) << b;
+    return 0;
+}
